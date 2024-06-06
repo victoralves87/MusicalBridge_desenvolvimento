f597ebd2a609d47b2a997c02097ba84a2bcfcb4c
Arthur Carvalho <arthur.carvalho.tech@gmail.com>
2024-06-06 10:06:18 -0300

    Atualizando docker

---
+++ access_db.sh
@@ -0,0 +1,1 @@
+sudo docker compose exec db mysql -u root -D banco_musical_bridge -pbanco123
---
+++ db.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL,
+  idade INT NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  senha VARCHAR(255) NOT NULL
+);
